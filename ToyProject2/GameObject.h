ddb640834dc4e0d1c34939ba3af7c1ab1627a889
JwangIn <trundle0912@naver.com>
2024-09-02 14:40:47 +0900

    2024-09-02

--- ToyProject2/GameObject.h
+++ ToyProject2/GameObject.h
@@ -3,10 +3,36 @@
 #include <string>
 #include <vector>
 
+class Bullet
+{
+private:
+	int range;
+public:
+	Bullet() {}
+	~Bullet(){}
+	void Shoot();
+};
+
+
 class Monster
 {
 private:
 	int HP;
-	int AD;
+	Bullet* bullet;
+public:
+	Monster(){}
+	~Monster(){}
+	void Attack();
+};
 
+class Player
+{
+private:
+	int HP;
+	Bullet* bullet;
+public:
+	Player(){}
+	~Player(){}
+	void Move();
+	void Attack();
 };
